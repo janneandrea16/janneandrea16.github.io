7ae708d3629cec35f389e96225c26b1bf6567efe
janneandrea16 <85207772+janneandrea16@users.noreply.github.com>
2021-06-22 14:47:35 -0500

    intent commit

--- weather/js/script.js
+++ weather/js/script.js
@@ -1,5 +1,14 @@
-const lastModified = document.lastModified;
-document.querySelector("#Date_Modified").textContent = lastModified;
+function toggleMenu() {
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
 
-const year = new Date().getFullYear();
- document.querySelector("#Current_Year").textContent = year;
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var currentDate = new Date();
+var day = currentDate.getDay();        
+var date = currentDate.getDate();
+var month = currentDate.getMonth();
+var year = currentDate.getFullYear();
+                           
+document.getElementById("displayDate").textContent = `${days[day]}, ${date} ${months[month]} ${year}`; 
