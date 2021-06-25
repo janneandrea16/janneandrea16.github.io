e714a2a2f11f22f7fedb07da15ab235bc97f9d59
janneandrea16 <85207772+janneandrea16@users.noreply.github.com>
2021-06-25 16:33:41 -0500

    intent commit

--- weather/js/script2.js
+++ weather/js/script2.js
@@ -1,3 +1,6 @@
 Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.”
 
-"block" in a selection structure where the condition looks at the day of the week through the Date() object and getDay() method.
+To change the display property in JavaScript, consider the use of .style.display = "block" in 
+a selection structure where the condition looks at the day of the week through the Date() object and getDay() method.
+
+date()
