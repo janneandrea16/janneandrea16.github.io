f09060e428f19806ad5a25fe8ae949528d980a1b
janneandrea16 <85207772+janneandrea16@users.noreply.github.com>
2021-06-25 16:27:10 -0500

    intent commit

---
+++ weather/js/script2.js
@@ -0,0 +1,3 @@
+Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.”
+
+"block" in a selection structure where the condition looks at the day of the week through the Date() object and getDay() method.
