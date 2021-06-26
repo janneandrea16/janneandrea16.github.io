e06892925b34a48c4a65349c4b775227239027ab
janneandrea16 <85207772+janneandrea16@users.noreply.github.com>
2021-06-26 16:18:36 -0500

    intent commit

--- weather/js/script2.js
+++ weather/js/script2.js
@@ -1,6 +1,6 @@
-Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.”
+//Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.”
 
-To change the display property in JavaScript, consider the use of .style.display = "block" in 
-a selection structure where the condition looks at the day of the week through the Date() object and getDay() method.
+//To change the display property in JavaScript, consider the use of .style.display = "block" in 
+//a selection structure where the condition looks at the day of the week through the Date() object and getDay() method.
 
-date()
+Date() getDay()
