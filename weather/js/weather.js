ce7eb0f1732ae3b398fb5248981a185ce454f178
janneandrea16 <85207772+janneandrea16@users.noreply.github.com>
2021-07-14 23:22:11 -0500

    intent commit

--- weather/js/weather.js
+++ weather/js/weather.js
@@ -93,3 +93,35 @@ fetch(weatherURL)
     document.getElementById("humidity").innerHTML = weathersummary.main.humidity;
     document.getElementById("wind_speed").innerHTML= weathersummary.wind.speed;
 });
+
+//Upcoming events
+
+for(let i=0; i< filter_town.length; i++){
+      
+    if (filter_town[i].name== page){
+
+        
+        let body = document.createElement('section');
+        body.classList.add("town");
+        let class_html=`<div class="event">
+        <h2>Upcoming Events:</h2>` 
+        for(const event of filter_town[i].events){
+            class_html+=`<p>${event}</p>`
+        }
+
+       class_html+= "</div>";
+        body.innerHTML= class_html;
+        
+        
+    body.setAttribute("class","events_section");
+
+    document.querySelector('div#events_').appendChild(body);
+    
+    }
+    
+   
+    
+
+}
+
+});
