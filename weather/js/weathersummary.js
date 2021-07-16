56966f9869865d78135c5fd0f953ae912eab293d
janneandrea16 <85207772+janneandrea16@users.noreply.github.com>
2021-07-16 01:03:39 -0500

    intent commit

--- weather/js/weathersummary.js
+++ weather/js/weathersummary.js
@@ -1,15 +1,13 @@
-let temperature = parseInt(document.getElementById("temperature").innerText);
-let wind_speed = parseInt(document.getElementById("wind_speed").innerText);
-let math_pow=Math.pow(wind_speed,0.16);
-let wind_chill = 35.74+(0.6215*temperature)-35.75*math_pow+(0.4275*(temperature*math_pow));
+//Weather summary on the hero image
+const temp = document.getElementById('temp').innerHTML;
+const speed = document.getElementById('speed').innerHTML;
 
 
-
-if (temperature<=50 && wind_speed > 3){
-    document.getElementById("wind").innerHTML = wind_chill.toFixed() + " °F";
-}
-
-else{
-
-document.getElementById("wind").innerHTML = "N/A"
-}
+let windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, .16)) + (0.4275 * temp * Math.pow(speed, .16));
+  if (temp <= 50 && speed > 3) {
+     windchill = Math.round(windchill);
+  } 
+  else {
+     windchill = "N/A";
+  }
+document.getElementById('windchill').innerHTML = windchill;
