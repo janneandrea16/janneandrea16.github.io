878254dd6b16ca19376c43fce81f2fab7c55947f
janneandrea16 <85207772+janneandrea16@users.noreply.github.com>
2021-07-24 18:21:50 -0500

    intent commit

--- scooter/js/rentals.js
+++ scooter/js/rentals.js
@@ -2,7 +2,7 @@ const apiURL = "https://mfuller45.github.io/final/data/scoots.json";
 fetch(url)
   .then((response) => response.json())
     .then((data) => {
-        data.rentals.forEach((rental, index) => {
+        data.rentals.forEach((rentals, index) => {
             document.getElementById('rhd' + (index + 1)).innerHTML = rental.half1;
             document.getElementById('rfd' + (index + 1)).innerHTML = rental.full1;
             document.getElementById('whd' + (index + 1)).innerHTML = rental.half2;
