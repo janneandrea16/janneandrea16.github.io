a27ec6dc92fe2ba8a5905a971079eb87fbe4bac3
janneandrea16 <85207772+janneandrea16@users.noreply.github.com>
2021-07-24 00:02:11 -0500

    intent commit

--- scooter/js/rentals.js
+++ scooter/js/rentals.js
@@ -14,7 +14,7 @@ function toggleMenu() {
   document.getElementById("copyright").innerHTML = year;
   
   
-  const url = "data.scoots.json";
+  const url = "https://mfuller45.github.io/final/data/scoots.json";
   fetch(url)
     .then((response) => response.json())
       .then((data) => {
